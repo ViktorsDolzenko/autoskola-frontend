4c28f797db5c1e885d3df31fa034f1192ff04806
AwSicker <vitja1234@gmail.com>
2022-09-10 02:03:06 +0300

    autoskola

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  sanity: {
+    projectId: '0ypps6d2',
+    dataset: 'production',
+    useCdn: true,
+  },
+  web: {
+    url: 'https://neon-sable-684157.netlify.app',
+  },
 };
