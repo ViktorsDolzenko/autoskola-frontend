189949831f4e3c253ee6abe712d2e5c37a71d5a3
AwSicker <vitja1234@gmail.com>
2022-09-12 21:09:29 +0300

    fix

--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -18,13 +18,13 @@ import {Router} from "@angular/router";
           [
             style({ height: 0, opacity: 0 }),
             animate('1s ease-out',
-              style({ height: 200, opacity: 1 }))
+              style({ height: 250, opacity: 1 }))
           ]
         ),
         transition(
           ':leave',
           [
-            style({ height: 200, opacity: 1 }),
+            style({ height: 250, opacity: 1 }),
             animate('1s ease-in',
               style({ height: 0, opacity: 0 }))
           ]
